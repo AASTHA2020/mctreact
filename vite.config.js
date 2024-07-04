346d29e5f75d604cd6e1bde76f80b26b38c0a482
AASTHA2020 <modiaastha0@gmail.com>
2024-07-04 18:07:49 +0530

    ok

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@mui/icons-material': '@mui/icons-material/esm'
+      '@mui/icons-material': '@mui/icons-material'
     }
   }
 });
